b241c1f1e13a29445e755ec5a914cfdcbadf811d
Alex Bennett <alexandergraybennett@gmail.com>
2019-12-19 12:34:37 -0700

    feat(App): Adds AssemblyScript to CRA

---
+++ assembly/index.ts
@@ -0,0 +1,11 @@
+export function add(a: i32, b: i32): i32 {
+  return a + b
+}
+
+var map = new Map<i32,i32>()
+
+for (let i: i32; i < data.length; i++) {
+  map.set(i, data[i].id)
+}
+
+export {map}
